f68f38838971979c3a25cbd1d4b7a1af13d3c8cf
Dancasiu Flavius <dancasiu.flavius@yahoo.com>
2023-11-08 22:49:14 +0200

    Test- Varianta -Flavius

---
+++ Test_Mostenire/Eticheta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Mostenire
+{
+    public class Eticheta
+    {
+        Dreptunghi _dreptunghi = new Dreptunghi();
+        String _text;
+
+        public Eticheta(Dreptunghi dreptunghi, string text)
+        {
+            _dreptunghi = dreptunghi;
+            _text = text;
+        }
+    }
+}
---
+++ Test_Mostenire/Punct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Mostenire
+{
+    public class Punct
+    {
+        private int _coordonataX;
+        private int _coordonataY;
+
+        public Punct(int coordonataX, int coordonataY)
+        {
+            _coordonataX = coordonataX;
+            _coordonataY = coordonataY;
+        }
+    }
+}
